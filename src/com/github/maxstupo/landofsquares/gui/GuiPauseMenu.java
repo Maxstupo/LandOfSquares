bd1d01f2347c64b5cbc7bb850da4c425e1caf5f7
Maxstupo <maxstupo@hotmail.com>
2016-12-03 22:48:40 +1100

    Added background for pause menu.

--- src/com/github/maxstupo/landofsquares/gui/GuiPauseMenu.java
+++ src/com/github/maxstupo/landofsquares/gui/GuiPauseMenu.java
@@ -1,6 +1,8 @@
 package com.github.maxstupo.landofsquares.gui;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 
 import com.github.maxstupo.flatengine.IEventListener;
 import com.github.maxstupo.flatengine.gui.GuiButton;
@@ -18,16 +20,18 @@ public class GuiPauseMenu extends AbstractGui implements IEventListener<GuiButto
 
     private static final Vector2i BTN_SIZE = new Vector2i(150, 25);
     private static final int BOARDER_SIZE = 10;
-    private GuiButton<State> btnQuit;
 
+    private GuiButton<State> btnQuit;
     private GuiNode<State> btnGroup;
 
     public GuiPauseMenu(AbstractGamestate<State> gamestate, String id) {
         super(gamestate, id);
 
         btnGroup = new GuiNode<>(gamestate);
+        btnGroup.getSize().set(BTN_SIZE.x + BOARDER_SIZE * 2, BTN_SIZE.y * 10);
+
         {
-            btnQuit = new GuiButton<>(gamestate, "Quit Game", null, BTN_SIZE);
+            btnQuit = new GuiButton<>(gamestate, "Quit Game", new Vector2i(BOARDER_SIZE, (BTN_SIZE.y * 9) - BOARDER_SIZE), BTN_SIZE);
             btnQuit.addListener(this);
 
             btnGroup.addChild(btnQuit);
@@ -38,6 +42,10 @@ public class GuiPauseMenu extends AbstractGui implements IEventListener<GuiButto
 
     @Override
     protected void render(Graphics2D g) {
+        Rectangle r = btnGroup.getBounds();
+
+        g.setColor(Color.LIGHT_GRAY);
+        g.fill3DRect(r.x, r.y, r.width, r.height, true);
 
         super.render(g);
     }
@@ -46,7 +54,7 @@ public class GuiPauseMenu extends AbstractGui implements IEventListener<GuiButto
     protected void onResize(int width, int height) {
         super.onResize(width, height);
 
-        btnGroup.setLocalPosition(width / 2 - BTN_SIZE.x / 2, height / 2);
+        btnGroup.setLocalPosition(width / 2 - BTN_SIZE.x / 2, height / 2 - btnGroup.getBounds().height / 2);
     }
 
     @Override
