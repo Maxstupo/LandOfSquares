b1c2bc87cfc97de62124f0955d634a04c402d7bc
Maxstupo <maxstupo@hotmail.com>
2016-12-11 22:36:02 +1100

    Added method doc.

--- src/com/github/maxstupo/landofsquares/world/lighting/LightingSystem.java
+++ src/com/github/maxstupo/landofsquares/world/lighting/LightingSystem.java
@@ -10,13 +10,14 @@ import com.github.maxstupo.landofsquares.world.block.Block;
 public class LightingSystem {
 
     private final World world;
-
+    private final int sunLightValue;
     private final LightingEngine lightingEngineSun;
     private final LightingEngine lightingEngineSourceBlocks;
 
     public LightingSystem(World world, int lightValueSun) {
         this.world = world;
 
+        this.sunLightValue = lightValueSun;
         this.lightingEngineSun = new LightingEngine(world, true, lightValueSun);
         this.lightingEngineSourceBlocks = new LightingEngine(world, false, lightValueSun);
     }
@@ -39,17 +40,24 @@ public class LightingSystem {
         lightingEngineSourceBlocks.removeTile(x, y);
     }
 
+    /**
+     * Returns a value between 0 and 1 that represents the light level at the specific x,y position. Factoring in both the sun and source block light
+     * levels as well as the time of day.
+     */
     public float getLightValue(int x, int y) {
 
         float daylight = world.getDaylight();
-        float lightValueSun = lightingEngineSun.getLightValue(x, y) / 15.0f * daylight;
-        float lightValueSourceBlocks = lightingEngineSourceBlocks.getLightValue(x, y) / 15.0f;
+        float lightValueSun = lightingEngineSun.getLightValue(x, y) / (float) sunLightValue * daylight;
+        float lightValueSourceBlocks = lightingEngineSourceBlocks.getLightValue(x, y) / (float) sunLightValue;
 
         return Math.max(lightValueSun, lightValueSourceBlocks);
     }
 
+    /**
+     * Returns a color alpha between 0-255 representing the light level at the specific x,y position.
+     */
     public int getLightValueConverted(int x, int y) {
-        int intensity = (int) (getLightValue(x, y) * 255);
+        int intensity = (int) (getLightValue(x, y) * 255f);
         return 255 - intensity;
     }
 
